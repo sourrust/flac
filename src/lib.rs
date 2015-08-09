e148a8e4f5375b88f8c7dd4139f5837ed212d87a
Jeremy Hull <sourdrums@gmail.com>
2015-08-08 18:07:12 -0700

    Export the parser to the main lib file

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
-#[test]
-fn it_works() {
+#[macro_use]
+extern crate nom;
+
+pub mod metadata;
+mod utility;
+
+use metadata::many_blocks;
+
+pub struct Stream<'a> {
+  pub meta_data: Vec<metadata::Block<'a>>,
+  //frames: Vec<u32>
 }
+
+named!(stream <&[u8], Stream>,
+  chain!(
+    tag!("fLaC") ~
+    blocks: many_blocks,
+    || {
+      Stream {
+        meta_data: blocks,
+      }
+    }
+  )
+);
