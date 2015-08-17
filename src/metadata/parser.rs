176d99897ef15c81f3188899cd0e4856b123e166
Jeremy Hull <sourdrums@gmail.com>
2015-08-16 19:05:05 -0700

    Use `to_owned` method to convert borrowed values

--- src/metadata/parser.rs
+++ src/metadata/parser.rs
@@ -79,7 +79,7 @@ named!(pub stream_info <&[u8], BlockData>,
         channels: channels + 1,
         bits_per_sample: bits_per_sample + 1,
         total_samples: total_samples,
-        md5_sum: md5_sum,
+        md5_sum: md5_sum.to_owned(),
       })
     }
   )
@@ -99,8 +99,8 @@ pub fn application(input: &[u8], length: u32) -> IResult<&[u8], BlockData> {
     data: take!(length - 4),
     || {
       BlockData::Application(Application {
-        id: id,
-        data: data,
+        id: id.to_owned(),
+        data: data.to_owned(),
       })
     }
   )
@@ -135,18 +135,18 @@ named!(pub vorbis_comment <&[u8], BlockData>,
     comments: count!(comment_field, number_of_comments as usize),
     || {
       BlockData::VorbisComment(VorbisComment {
-        vendor_string: vendor_string,
+        vendor_string: vendor_string.to_owned(),
         comments: comments,
       })
     }
   )
 );
 
-named!(comment_field <&[u8], &str>,
+named!(comment_field <&[u8], String>,
   chain!(
     comment_length: le_u32 ~
     comment: take_str!(comment_length),
-    || { comment }
+    || { comment.to_owned() }
   )
 );
 
@@ -161,7 +161,7 @@ named!(pub cue_sheet <&[u8], BlockData>,
       let is_cd = ((bytes[0] >> 7) & 0b01) == 1;
 
       BlockData::CueSheet(CueSheet {
-        media_catalog_number: media_catalog_number,
+        media_catalog_number: media_catalog_number.to_owned(),
         lead_in: lead_in,
         is_cd: is_cd,
         tracks: tracks,
@@ -188,10 +188,10 @@ named!(cue_sheet_track <&[u8], CueSheetTrack>,
       CueSheetTrack {
         offset: offset,
         number: number,
-        isrc: isrc,
+        isrc: isrc.to_owned(),
         isnt_audio: isnt_audio,
         is_pre_emphasis: is_pre_emphasis,
-        indices: indices.unwrap_or(Vec::new()),
+        indices: indices.unwrap_or(vec![]),
       }
     }
   )
@@ -252,13 +252,13 @@ named!(pub picture <&[u8], BlockData>,
 
       BlockData::Picture(Picture {
         picture_type: picture_type,
-        mime_type: mime_type,
-        description: description,
+        mime_type: mime_type.to_owned(),
+        description: description.to_owned(),
         width: width,
         height: height,
         depth: depth,
         colors: colors,
-        data: data,
+        data: data.to_owned(),
       })
     }
   )
@@ -268,7 +268,8 @@ named!(pub picture <&[u8], BlockData>,
 // `Block`s but actually 7 that are implemented. When the `Block` type isn't
 // recognised, this block gets skipped over with this parser.
 pub fn unknown(input: &[u8], length: u32) -> IResult<&[u8], BlockData> {
-  map!(input, take!(length), BlockData::Unknown)
+  map!(input, take!(length), |data: &[u8]|
+    BlockData::Unknown(data.to_owned()))
 }
 
 named!(pub header <&[u8], (bool, u8, u32)>,
