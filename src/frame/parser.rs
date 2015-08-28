e00dbe2cc703c7505917a54ef67fe024f0fffc5f
Jeremy Hull <sourdrums@gmail.com>
2015-08-27 19:18:15 -0700

    Add parser for up to blocking strategy
    Parses these parts of the frame header:
    
    * Sync code
    * reserved bit (must be zero)
    * blocking strategy

--- src/frame/parser.rs
+++ src/frame/parser.rs
@@ -1,5 +1,29 @@
-use nom::be_u16;
+use nom::{
+  be_u16,
+  IResult,
+  ErrorCode, Err,
+};
 
 use frame::Footer;
+
+fn blocking_strategy(input: &[u8]) -> IResult<&[u8], bool> {
+  match take!(input, 2) {
+    IResult::Done(i, bytes)   => {
+      let sync_code = ((bytes[0] as u16) << 6) +
+                      (bytes[1] as u16) >> 2;
+      let is_valid  = ((bytes[1] >> 1) & 0b01) == 0;
+
+      if sync_code == 0b11111111111110 && is_valid {
+        let is_variable_block_size = (bytes[1] & 0b01) == 1;
+
+        IResult::Done(i, is_variable_block_size)
+      } else {
+        IResult::Error(Err::Position(ErrorCode::Digit as u32, input))
+      }
+    }
+    IResult::Error(error)     => IResult::Error(error),
+    IResult::Incomplete(need) => IResult::Incomplete(need),
+  }
+}
 
 named!(footer <&[u8], Footer>, map!(be_u16, Footer));
