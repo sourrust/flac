bf7cfdc0b1173c6d9e3bdc5f77eb24f53368e321
Jeremy Hull <sourdrums@gmail.com>
2015-08-27 19:42:40 -0700

    Add parser to figure out the size of utf8 variable
    This can be from 1 to 7 bytes, including the the current byte being parsed.

--- src/frame/parser.rs
+++ src/frame/parser.rs
@@ -1,5 +1,5 @@
 use nom::{
-  be_u16,
+  be_u8, be_u16,
   IResult,
   ErrorCode, Err,
 };
@@ -69,6 +69,22 @@ fn channel_bits(input: &[u8]) -> IResult<&[u8], (ChannelAssignment, usize)> {
     IResult::Error(error)     => IResult::Error(error),
     IResult::Incomplete(need) => IResult::Incomplete(need),
   }
+}
+
+fn utf8_size(input: &[u8], is_u64: bool)
+             -> IResult<&[u8], Option<(usize, u8)>> {
+  map!(input, be_u8, |utf8_header| {
+    match utf8_header {
+      0b00000000...0b01111111 => Some((0, utf8_header)),
+      0b11000000...0b11011111 => Some((1, utf8_header & 0b00011111)),
+      0b11100000...0b11101111 => Some((2, utf8_header & 0b00001111)),
+      0b11110000...0b11110111 => Some((3, utf8_header & 0b00000111)),
+      0b11111000...0b11111011 => Some((4, utf8_header & 0b00000011)),
+      0b11111100...0b11111101 => Some((5, utf8_header & 0b00000001)),
+      0b11111110              => if is_u64 { Some((6, 0)) } else { None },
+      _                       => None,
+    }
+  })
 }
 
 named!(footer <&[u8], Footer>, map!(be_u16, Footer));
