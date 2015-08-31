5aa69bbcb9344728d3d19f9697b80d4bb856f9b9
Jeremy Hull <sourdrums@gmail.com>
2015-08-30 18:06:10 -0700

    Parse and add frames to stream parser

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ mod frame;
 mod utility;
 
 use metadata::metadata_parser;
+use frame::frame_parser;
 
 pub struct Stream {
   pub info: metadata::StreamInfo,
@@ -16,10 +17,12 @@ pub struct Stream {
 named!(stream <&[u8], Stream>,
   chain!(
     blocks: metadata_parser ~
+    frames: many1!(apply!(frame_parser, blocks.0.channels)),
     move|| {
       Stream {
         info: blocks.0,
         metadata: blocks.1,
+        frames: frames,
       }
     }
   )
