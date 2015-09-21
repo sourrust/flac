bcf159fc5d268b581e4f222926f57dfa3273cea8
Jeremy Hull <sourdrums@gmail.com>
2015-09-20 20:41:05 -0700

    Add documentation for Frame fields

--- src/frame/types.rs
+++ src/frame/types.rs
@@ -3,7 +3,9 @@ pub const MAX_CHANNELS: usize = 8;
 
 /// Audio frame that contains one sample for each channel.
 pub struct Frame {
+  /// Information regarding the current audio frame.
   pub header: Header,
+  /// CRC-16 of all frame bytes before this footer.
   pub footer: Footer,
 }
 
