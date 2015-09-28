0789fd3af28f01dbc1f82924d07149b77b22b01c
Jeremy Hull <sourdrums@gmail.com>
2015-09-27 18:38:54 -0700

    Add parser for leading zeros
    This is a decoding a unary formated number. However, "leading zeroes" seemed
    like a better name and this is for both finding the (wasted bits - 1) along
    with figuring out the number of bits in the encoded residuals of golomb-rice
    coding.

--- src/frame/subframe/parser.rs
+++ src/frame/subframe/parser.rs
@@ -1,7 +1,48 @@
 use nom::{
   IResult,
   ErrorCode, Err,
+  Needed,
 };
+
+fn leading_zeros(input: (&[u8], usize)) -> IResult<(&[u8], usize), u32> {
+  let (bytes, mut offset) = input;
+
+  let mut index     = 0;
+  let mut count     = 0;
+  let mut is_parsed = false;
+  let bytes_len     = bytes.len();
+
+  for i in 0..bytes_len {
+    // Clear the number of offset bits
+    let byte  = bytes[i] << offset;
+    let zeros = byte.leading_zeros() as usize;
+
+    if byte > 0 {
+      index     = i;
+      is_parsed = true;
+      count    += zeros;
+      offset   += zeros + 1;
+
+      if offset >= 8 {
+        index  += 1;
+        offset -= 8;
+      }
+
+      break;
+    } else {
+      count += zeros - offset;
+      offset = 0;
+    }
+  }
+
+  if is_parsed {
+    IResult::Done((&bytes[index..], offset), count as u32)
+  } else if index + 1 > bytes_len {
+    IResult::Incomplete(Needed::Size(index + 1))
+  } else {
+    IResult::Error(Err::Position(ErrorCode::TakeUntil as u32, bytes))
+  }
+}
 
 fn header(input: (&[u8], usize)) -> IResult<(&[u8], usize), (u8, bool)> {
   let result = chain!(input,
