6c96c40ac0f786a70be5f606262bd334e9c21541
Jeremy Hull <sourdrums@gmail.com>
2015-10-03 19:50:46 -0700

    Refactor subframe header parsing

--- src/frame/subframe/parser.rs
+++ src/frame/subframe/parser.rs
@@ -69,21 +69,11 @@ pub fn subframe_parser<'a>(input: (&'a [u8], usize),
 }
 
 fn header(input: (&[u8], usize)) -> IResult<(&[u8], usize), (u8, bool)> {
-  let result = chain!(input,
-    bit_padding: take_bits!(u8, 1) ~
-    subframe_type: take_bits!(u8, 6) ~
-    wasted_bit_flag: take_bits!(u8, 1),
-    || {
-      let is_valid        = bit_padding == 0;
-      let has_wasted_bits = wasted_bit_flag == 1;
-
-      (is_valid, subframe_type, has_wasted_bits)
-    }
-  );
-
-  match result {
-    IResult::Done(i, data)    => {
-      let (is_valid, subframe_type, has_wasted_bits) = data;
+  match take_bits!(input, u8, 8) {
+    IResult::Done(i, byte)    => {
+      let is_valid        = (byte >> 7) == 0;
+      let subframe_type   = (byte >> 1) & 0b111111;
+      let has_wasted_bits = (byte & 0b01) == 1;
 
       if is_valid {
         IResult::Done(i, (subframe_type, has_wasted_bits))
