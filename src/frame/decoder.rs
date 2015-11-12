1a0e2e1056eb8bccd50f7c7e07b58cb3e4e0ef53
Jeremy Hull <sourdrums@gmail.com>
2015-11-11 19:05:13 -0700

    Add implementation for `LeftSide` decoding

--- src/frame/decoder.rs
+++ src/frame/decoder.rs
@@ -1,5 +1,17 @@
 use frame::ChannelAssignment;
 
+fn decode_left_side(buffer: &mut [i32]) {
+  let block_size = buffer.len() / 2;
+
+  for i in 0..block_size {
+    let left = buffer[i];
+    let side = buffer[i + block_size];
+
+    // right channel
+    buffer[i + block_size] = left - side;
+  }
+}
+
 pub fn decode(channel_assignment: ChannelAssignment, buffer: &mut [i32]) {
   match channel_assignment {
     ChannelAssignment::Independent => return,
