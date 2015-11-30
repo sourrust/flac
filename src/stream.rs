b3c65ef04495e04506a1e6493744c355afd97dad
Jeremy Hull <sourdrums@gmail.com>
2015-11-29 21:17:56 -0700

    Add the two different states to `Stream`

--- src/stream.rs
+++ src/stream.rs
@@ -1,3 +1,6 @@
+use nom::{ConsumerState, Move};
+
+
 use metadata::{Metadata, StreamInfo, metadata_parser};
 use frame::{frame_parser, Frame};
 
@@ -11,6 +14,8 @@ pub struct Stream {
   pub info: StreamInfo,
   pub metadata: Vec<Metadata>,
   pub frames: Vec<Frame>,
+  state: ParserState,
+  consumer_state: ConsumerState<(), ErrorKind, Move>,
 }
 
 named!(pub stream_parser <&[u8], Stream>,
@@ -18,10 +23,14 @@ named!(pub stream_parser <&[u8], Stream>,
     blocks: metadata_parser ~
     frames: many1!(apply!(frame_parser, &blocks.0)),
     move|| {
+      let consumed = Move::Consume(0);
+
       Stream {
         info: blocks.0,
         metadata: blocks.1,
         frames: frames,
+        state: ParserState::Marker,
+        consumer_state: ConsumerState::Continue(consumed),
       }
     }
   )
