4a6757f8a7a2486fe24c1b7b6cbf889b9851b5c1
Jeremy Hull <sourdrums@gmail.com>
2015-12-04 20:49:25 -0700

    Add a growable buffer for dealing with `Read`s

--- src/utility/types.rs
+++ src/utility/types.rs
@@ -1,5 +1,7 @@
 use nom::{IResult, Needed};
 
+use std::ptr;
+
 pub enum ErrorKind {
   EndOfInput,
   Unknown,
@@ -58,5 +60,88 @@ impl<'a> StreamProducer for ByteStream<'a> {
       }
       IResult::Error(_)      => Err(ErrorKind::Unknown),
     }
+  }
+}
+
+pub struct Buffer {
+  data: Vec<u8>,
+  filled: usize,
+  offset: usize,
+}
+
+impl Buffer {
+  pub fn new() -> Self {
+    Self::with_capacity(1024)
+  }
+
+  pub fn with_capacity(capacity: usize) -> Self {
+    let mut buffer = Vec::with_capacity(capacity);
+
+    unsafe {
+      buffer.set_len(capacity);
+    }
+
+    Buffer {
+      data: buffer,
+      filled: 0,
+      offset: 0,
+    }
+  }
+
+  #[inline]
+  pub fn len(&self) -> usize {
+    self.filled - self.offset
+  }
+
+  #[inline]
+  pub fn is_empty(&self) -> bool {
+    self.len() == 0
+  }
+
+  #[inline]
+  pub fn capacity(&self) -> usize {
+    self.data.len()
+  }
+
+  pub fn as_slice(&self) -> &[u8] {
+    &self.data[self.offset..self.filled]
+  }
+
+  pub fn fill<R: Read>(&mut self, reader: &mut R) -> io::Result<usize> {
+    reader.read(&mut self.data[self.filled..]).map(|consumed| {
+      self.filled += consumed;
+
+      consumed
+    })
+  }
+
+  pub fn resize(&mut self, size: usize) {
+    if size > self.data.capacity() {
+      self.data.reserve(size);
+
+      let capacity = self.data.capacity();
+
+      unsafe {
+        self.data.set_len(capacity);
+      }
+    }
+
+    if self.data.len() - self.filled < size  {
+      let length      = self.filled - self.offset;
+      let mut mut_ptr = self.data.as_mut_ptr();
+
+      unsafe {
+        let offset_ptr  = self.data.as_ptr().offset(self.offset as isize);
+
+        ptr::copy(offset_ptr, mut_ptr, length);
+      }
+
+      self.filled -= self.offset;
+      self.offset  = 0;
+    }
+  }
+
+  pub fn consume(&mut self, consumed: usize) {
+    self.offset += consumed;
   }
 }
