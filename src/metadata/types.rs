e02f92039c918e551d7034a5335eb68788d80df0
Jeremy Hull <sourdrums@gmail.com>
2015-12-07 13:23:38 -0700

    Rewrite `handle_marker` for what it actually need

--- src/metadata/types.rs
+++ src/metadata/types.rs
@@ -253,24 +253,16 @@ impl MetaDataConsumer {
   }
 
   fn handle_marker<'a>(&mut self, input: &'a [u8]) -> IResult<&'a [u8], ()> {
+    let kind = ErrorKind::Custom(0);
+
     match tag!(input, "fLaC") {
       IResult::Done(i, _)    => {
-        let offset   = input.offset(i);
-        let consumed = Move::Consume(offset);
+        self.state = ParserState::Header;
 
-        self.state          = ParserState::Header;
-        self.consumer_state = ConsumerState::Continue(consumed);
+        IResult::Error(Err::Position(kind, i))
       }
-      IResult::Error(_)      => {
-        let kind = ErrorKind::Custom(0);
-
-        self.consumer_state = ConsumerState::Error(kind);
-      }
-      IResult::Incomplete(_) => {
-        let needed = Move::Await(Needed::Size(4));
-
-        self.consumer_state = ConsumerState::Continue(needed);
-      }
+      IResult::Error(_)      => IResult::Error(Err::Code(kind)),
+      IResult::Incomplete(n) => IResult::Incomplete(n),
     }
   }
 
