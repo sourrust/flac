2b377c5678a74811acd478f512f0df9ba241129b
Jeremy Hull <sourdrums@gmail.com>
2015-12-23 16:47:25 -0700

    Reduce iteration comparison
    Since both `sample_index` and `block_size` are zero on initiation, there
    isn't a need to check if `block_size` is zero.

--- src/stream.rs
+++ src/stream.rs
@@ -268,7 +268,7 @@ impl<'a> Iterator for Iter<'a> {
   type Item = i32;
 
   fn next(&mut self) -> Option<Self::Item> {
-    if self.block_size == 0 || self.sample_index == self.block_size {
+    if self.sample_index == self.block_size {
       let frame_index = self.stream.frame_index;
 
       if self.stream.next_frame().is_none() {
