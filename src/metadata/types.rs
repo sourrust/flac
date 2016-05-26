0ff34c7253135a995d051fd40f75a88bb2f5a90f
Jeremy Hull <sourdrums@gmail.com>
2016-05-25 21:00:17 -0700

    Switch the top level Application to Write trait

--- src/metadata/types.rs
+++ src/metadata/types.rs
@@ -137,15 +137,13 @@ impl Metadata {
       }
       Data::Application(ref application)      => {
         let length    = application.bytes_len();
-        let mut bytes = vec![0; 4 + length];
+        let mut bytes = Vec::with_capacity(4 + length);
 
-        bytes[0] = byte + 2;
+        bytes.write_u8(byte + 2);
 
-        bytes[1] = (length >> 16) as u8;
-        bytes[2] = (length >> 8) as u8;
-        bytes[3] = length as u8;
+        bytes.write_be_u24(length as u32);
 
-        application.to_bytes_buffer(&mut bytes[4..]);
+        application.to_bytes(&mut bytes);
 
         bytes
       }
