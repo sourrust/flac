e13eb0af48f11b76e4d5603a6588e095688fad27
Jeremy Hull <sourdrums@gmail.com>
2016-06-05 20:08:55 -0700

    Remove the `EndOfInput` match entirely
    The last branch for Err(_) will be the catch all including one for the
    `ErrorKind::EndOfInput`.

--- src/stream.rs
+++ src/stream.rs
@@ -135,7 +135,7 @@ impl<P> Stream<P> where P: StreamProducer {
 
     loop {
       match self.producer.parse(|i| frame_parser(i, stream_info, buffer)) {
-        Ok(frame)                  => {
+        Ok(frame)                => {
           let channels    = frame.header.channels as usize;
           let block_size  = frame.header.block_size as usize;
           let mut channel = 0;
@@ -154,9 +154,8 @@ impl<P> Stream<P> where P: StreamProducer {
 
           return Some(block_size);
         }
-        Err(ErrorKind::Continue)   => continue,
-        Err(ErrorKind::EndOfInput) |
-        Err(_)                     => return None,
+        Err(ErrorKind::Continue) => continue,
+        Err(_)                   => return None,
       }
     }
   }
